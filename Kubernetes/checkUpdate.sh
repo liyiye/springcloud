83b403c6f8d98707f7c8be3d1bb180a3a8b402f1
liyiye <yiye434@qq.com>
2019-05-16 16:51:38 +0800

    添加发布失败后的回滚操作

--- Kubernetes/checkUpdate.sh
+++ Kubernetes/checkUpdate.sh
@@ -14,16 +14,29 @@ echo "更新的数量：${count}"
 if [ $totalnum -eq $count ];
 then
     exit 0;
-    break;
 else
    ((checkcount=checkcount-1))
-
 fi
-
 done
 
 if [ $checkcount  -eq 0  ];
 then
- echo "发布失败"
- exit 1;
+ echo "发布失败，进行回滚,失败原因："
+ echo `kubectl get pods -o=jsonpath='{.items[*].status.containerStatuses[*].state.*.message}'`
+  echo "进行回滚："
+ `kubectl rollout undo deployment/$1`
+  checkcount=20
+while [ $checkcount -gt 0 ];
+do
+sleep 1s
+count=`kubectl get pods -o=jsonpath='{.items[*].status.containerStatuses[*].state}'|grep -o running |wc -l`
+echo "回滚的数量：${count}"
+if [ $totalnum -eq $count ];
+then
+    echo "回滚成功"
+    exit 1;
+else
+   ((checkcount=checkcount-1))
+fi
+done
 fi
