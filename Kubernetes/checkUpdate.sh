bec988a336e636726db91eb9cdcac1652338eb21
liyiye <yiye434@qq.com>
2019-05-16 16:02:38 +0800

    添加校验更新版本是否成功的脚本

---
+++ Kubernetes/checkUpdate.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#传入的第一个变量是deployment的名称，第二个变量是要检查的镜像的名称
+totalnum=`kubectl get deployment $1 -o=jsonpath='{.spec.replicas}'`
+echo "总的镜像的数量：${totalnum}"
+echo "更新的版本：${2}"
+checkcount=20
+while [ $checkcount -gt 0 ];
+do
+
+sleep 1s
+
+count=`kubectl get pods -o=jsonpath='{.items[*].spec.containers[*].image}' |grep -o $2 |wc -l`
+echo "更新的数量：${count}"
+if [ $totalnum -eq $count ];
+then
+    exit 0;
+    break;
+else
+   ((checkcount=checkcount-1))
+
+fi
+
+done
+
+if [ $checkcount -gt 0  ];
+then
+ echo "发布失败"
+ exit1;
+fi
